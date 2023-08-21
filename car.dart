22f25d993b66ab902ae3e362a459225051448782
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-08-21 15:19:46 +0800

    instance of class in other class property

--- car.dart
+++ car.dart
@@ -1,13 +1,12 @@
 void main() {
-  Car car1 = new Car("Sedan", "Honda", cc: 2.0, color: "Red");
-  Car car2 = new Car("MPV", "Toyota", year: 2018, color: "Blue");
-  Car car3 = new Car(
-    "Sedan",
-    "Honda",
-    color: "Black",
-    cc: 1.8,
-    year: 2017,
-  );
+  Owner owner1 = new Owner('88888888', name: 'Zaid', gender: 'Male', age: 35);
+  Owner owner2 =
+      new Owner('77777777', name: 'Zainab', gender: 'Female', age: 35);
+
+  Car car1 = new Car("Sedan", "Honda", cc: 2.0, color: "Red", owner: owner1);
+  Car car2 = new Car("MPV", "Toyota", year: 2018, color: "Blue", owner: owner1);
+  Car car3 = new Car("Sedan", "Honda",
+      color: "Black", cc: 1.8, year: 2017, owner: owner2);
 
   car1.printCar();
   car2.printCar();
@@ -20,8 +19,10 @@ class Car {
   int? year;
   String color;
   String brand;
+  Owner? owner;
 
-  Car(this.model, this.brand, {this.cc, this.year, required this.color});
+  Car(this.model, this.brand,
+      {this.cc, this.year, required this.color, this.owner});
 
   void printCar() {
     print("Model: " + model);
@@ -29,6 +30,20 @@ class Car {
     print("Year: " + year.toString());
     print("Color: " + color);
     print("Brand: " + brand);
+    print("**********************");
+    print("Owner IC: " + owner!.ic);
+    print("Owner Name: " + owner!.name);
+    print("Owner Gender: " + owner!.gender);
+    print("Owner Age: " + owner!.age.toString());
     print("======================");
   }
+}
+
+class Owner {
+  String name;
+  int age;
+  String gender;
+  String ic;
+
+  Owner(this.ic, {required this.name, required this.gender, required this.age});
 }
